ce07d5b8387cd2927c2c2608d63fd9d74faa3161
rottenpeachblossom <2269448093@qq.com>
2018-08-21 23:21:07 +0800

    hero类 和 动画资源加载

--- src/obj/hero/Hero.js
+++ src/obj/hero/Hero.js
@@ -1,8 +1,46 @@
 /*
-* name;
+* hero 基类
 */
-var Hero = (function () {
+var Hero = (function (_super) {
     function Hero() {
+        Hero.super(this);
+
+        this.maxHp = 1;
+        this.hp = 1;
+        this.atk = 1;
+
+    }
+
+    Laya.class(Hero, "Hero", _super);
+
+    var _proto = Hero.prototype;
+
+    _proto.init = function(){
+        //创建一个动画为飞机的身体
+        this.body = new Laya.Animation();
+        //把机体添加到 容器内
+        this.addChild(this.body);
+        this.body.on(Laya.Event.COMPLETE, this, this.onPlayComplete);
+        this.playAction("fly");
+    }
+    _proto.onPlayComplete = function(){
+        //如果是击毁动画 则隐藏对象
+        if(this.action === "down"){
+            //停止动画播放
+            this.body.stop();
+            //隐藏显示
+            this.visible = false;
+        }else if(this.action === "hit"){
+            this.playAction("fly");
+        }
+    }
+    _proto.playAction = function(action){
+         this.action = action;
+        this.body.play(0, true, this.type + "_" + action);
+        //获取动画大小区域t
+        this.bound = this.body.getBounds();
+        //设置机身居中
+        this.body.pos(-this.bound.width/2, -this.bound.height/2);
     }
     return Hero;
-}());
+}(Laya.Sprite));
--- src/scene/LoadScene.js
+++ src/scene/LoadScene.js
@@ -28,6 +28,7 @@ var LoadScene = (function (_super) {
 		progressBar.sizeGrid = "5,5,5,5";
 		Laya.stage.addChild(progressBar);
 		this.startLoad();
+		this.loadAnimation();
 	}
 
 	/**
@@ -68,7 +69,47 @@ var LoadScene = (function (_super) {
 		progressBar.value = percent;
 		// console.log('当前进度:' + percent);
 	}
+    
+	/**
+	 * 动画加载
+	 */
+    _proto.loadAnimation = function(){
+        //缓存飞机的动作
+        Laya.Animation.createFrames(["resource/hero_fly1.png","resource/hero_fly2.png"],"hero_fly");
+        //缓存集中爆炸动作
+        Laya.Animation.createFrames(["resource/hero_down1.png","resource/hero_down2.png"
+        ,"resource/hero_down3.png","resource/hero_down4.png"],"hero_down");
 
+        //缓存敌机1飞行动作
+        Laya.Animation.createFrames(["resource/enemy1_fly1.png"],"enemy1_fly");
+        //缓存敌机1爆炸动作
+        Laya.Animation.createFrames(["resource/enemy1_down1.png","resource/enemy1_down2.png","resource/enemy1_down3.png"
+        ,"resource/enemy1_down4.png"],"enemy1_down");
+
+        //缓存敌机2飞行动作
+        Laya.Animation.createFrames(["resource/enemy2_fly1.png"],"enemy2_fly");
+        //缓存敌机2爆炸动作
+        Laya.Animation.createFrames(["resource/enemy2_down1.png","resource/enemy2_down2.png","resource/enemy2_down3.png"
+        ,"resource/enemy2_down4.png"],"enemy2_down");
+        //缓存敌机2碰撞动作
+        Laya.Animation.createFrames(["resource/enemy2_hit.png"],"enemy2_hit");
+
+        //缓存敌机3飞行动作
+        Laya.Animation.createFrames(["resource/enemy3_fly1.png","resource/enemy3_fly2.png"],"enemy3_fly");
+        //缓存敌机3爆炸动作
+        Laya.Animation.createFrames(["resource/enemy3_down1.png","resource/enemy3_down2.png","resource/enemy3_down3.png"
+        ,"resource/enemy3_down4.png","resource/enemy3_down5.png","resource/enemy3_down6.png"],"enemy3_down");
+        //缓存敌机3碰撞动作
+        Laya.Animation.createFrames(["resource/enemy3_hit.png"],"enemy3_hit");
+
+        //缓存子弹动画
+        Laya.Animation.createFrames(["resource/bullet1.png"],"bullet1_fly");
+
+        //缓存强化包
+        Laya.Animation.createFrames(["resource/ufo1.png"],"ufo1_fly");
+        //缓存医疗包
+        Laya.Animation.createFrames(["resource/ufo2.png"],"ufo2_fly");
+    }
 	return LoadScene;
 
 }(Laya.Sprite));
